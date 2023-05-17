db1ac48d573444a5bb603e5b21dd626cb8a29e4d
Deshy Dan <deshy733@gmail.com>
2023-05-17 17:27:50 +0200

    Added Memento pattern

---
+++ src/Memento/com/History.java
@@ -0,0 +1,20 @@
+package Memento.com;
+
+import java.util.Stack;
+
+public class History {
+    Stack<EditorState> stack = new Stack<>();
+
+    public void add(EditorState state) {
+        stack.push(state);
+    }
+
+    public EditorState getLastest() {
+        return stack.peek();
+    }
+
+    public EditorState restoreLast() {
+        stack.pop();
+        return stack.peek();
+    }
+}
